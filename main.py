bfe173b1e5503453a709dc413532dc259709c5bb
Pauline Cha <pacha@davidson.edu>
2023-12-14 23:34:53 -0500

    dont change

--- main.py
+++ main.py
@@ -151,6 +151,7 @@ def get_display():
     inserted_users = set()
     inserted_playlists = set()
     inserted_tracks = set()
+    user=dict()
     try:
         user = sp.current_user()
     except spotipy.SpotifyException as error:
